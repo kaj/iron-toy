dd9bc945bedfa22e8a2298d5ede8d81408d6aa71
Rasmus Kaj <kaj@kth.se>
2015-09-06 14:51:43 +0200

    Cleaned-up args + render macro.

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,38 @@ use iron::status;
 use iron::mime::Mime;
 use router::Router;
 
+macro_rules! render {
+    ($template:expr, $($name:ident=$arg:tt),*) => {{
+        let html = "text/html".parse::<Mime>().unwrap();
+        Ok(Response::with((status::Ok, html,
+                           format!($template, $($name=$arg),*))))
+    }};
+}
+
 fn main() {
     fn hello_world(req: &mut Request) -> IronResult<Response> {
-        let html = "text/html".parse::<Mime>().unwrap();
-        let ref query = req.extensions.get::<Router>()
-            .unwrap().find("query").unwrap_or("World");
-        Ok(Response::with((status::Ok, html,
-                           format!("<!doctype html>\nHello {}!", query))))
+        let (who,) = {
+            let args = req.extensions.get::<Router>().unwrap();
+            (args.find("who").unwrap_or("World").to_string(),)
+        };
+        render!("<!doctype html>\nHello {who}!", who=who)
     }
     fn other(req: &mut Request) -> IronResult<Response> {
-        let html = "text/html".parse::<Mime>().unwrap();
-        let ref x = req.extensions.get::<Router>()
-            .unwrap().find("x").unwrap_or("Client");
-        Ok(Response::with((status::Ok, html,
-                           format!("<!doctype html>\nHello {}!", x))))
+        let (x, y) = {
+            let args = req.extensions.get::<Router>().unwrap();
+            ( args.find("x").unwrap_or("World").to_string(),
+              args.find("y").unwrap_or("walk").to_string(),
+              )
+        };
+        render!("<!doctype html>\nHello {who}!<p>Fine day for a {what}.",
+                who=x, what=y)
     }
     Iron::new(router!(
         get "/" => hello_world,
-        get "/foo/:query" => hello_world,
-        get "/bar/:x" => other
+        get "/foo/:who" => hello_world,
+        get "/bar/" => other,
+        get "/bar/:x" => other,
+        get "/bar/:x/:y" => other
         )).http("localhost:3000").unwrap();
     println!("On 3000");
 }
