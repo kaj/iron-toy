9de9668304e8f50453016f051b13cff1eddc5052
Rasmus Kaj <kaj@kth.se>
2015-09-16 22:49:01 +0200

    Use handlebars-iron rather than liquid.
    There is a templating framework that allready works as an iron
    extension, so use it.

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,21 @@
 extern crate iron;
 #[macro_use]
 extern crate router;
-extern crate liquid;
+extern crate handlebars_iron as hbs;
+extern crate rustc_serialize;
 
+use std::collections::BTreeMap;
 use iron::prelude::*;
 use iron::status;
-use iron::mime::Mime;
 use router::Router;
-use std::fs::File;
-use std::io::Read;
-use liquid::LiquidOptions;
-use liquid::Renderable;
-use liquid::Context;
-use liquid::Value;
-use liquid::Template;
-use liquid::parse;
-
-fn load_template(name: &str) -> Result<Template, String> {
-    let filename = format!("./templates/{}.html", name);
-    let mut text = String::new();
-    let _x = File::open(filename).unwrap().read_to_string(&mut text);
-    let mut options : LiquidOptions = Default::default();
-    parse(&text, &mut options)
-}
+use hbs::{Template, HandlebarsEngine};
+use rustc_serialize::json::ToJson;
 
 macro_rules! render {
     ($template:expr, $($name:ident=$arg:expr),*) => {{
-        //println!("Should render {:?} {:?}", $template, vec!($(stringify!($name)),*));
-        let html = "text/html".parse::<Mime>().unwrap();
-        let template = match load_template($template) {
-            Ok(result) => result,
-            Err(e) => panic!("Failed to parse template {:?}: {}", $template, e)
-        };
-
-        let mut data = Context::new();
-        $(
-            data.set_val(stringify!($name), $arg);
-           )*
-        match template.render(&mut data) {
-            Some(text) => Ok(Response::with((status::Ok, html, text.to_string()))),
-            None => panic!("Failed to render template {}", $template)
-        }
+        let mut data = BTreeMap::new();
+        $( data.insert(stringify!($name).to_string(), $arg.to_json()); )*
+        Ok(Response::with((status::Ok, Template::new($template, data))))
     }};
 }
 
@@ -50,8 +25,9 @@ fn main() {
             let args = req.extensions.get::<Router>().unwrap();
             (args.find("who").unwrap_or("World").to_string(),)
         };
-        render!("hello", who=Value::Str(who))
+        render!("hello", who=who)
     }
+
     fn other(req: &mut Request) -> IronResult<Response> {
         let (x, y) = {
             let args = req.extensions.get::<Router>().unwrap();
@@ -59,15 +35,18 @@ fn main() {
               args.find("y").unwrap_or("walk").to_string(),
               )
         };
-        render!("hello", who=Value::Str(x), what=Value::Str(y))
+        render!("hello", who=x, what=y)
     }
-    let app = Iron::new(router!(
+
+    let mut chain = Chain::new(router!(
         get "/" => hello_world,
         get "/foo/:who" => hello_world,
         get "/bar/" => other,
         get "/bar/:x" => other,
         get "/bar/:x/:y" => other
-    ));
+            ));
+    chain.link_after(HandlebarsEngine::new("./templates/", ".hbs"));
+    let app = Iron::new(chain);
     let addr = "localhost:3000";
     println!("Running on http://{}", addr);
     app.http("localhost:3000").unwrap();
